b6a550ac3a9dd05d17ba51e69ac3667c90eed748
Jim.Campbell <jim.campbell@canoo.com>
2024-10-26 19:15:31 -0400

    project properties file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,22 @@
-import com.android.build.gradle.internal.utils.isKotlinKaptPluginApplied
+import java.util.Properties
+
+val projectPropertiesPath = "../project.properties"
+val properties = readProperties(file(projectPropertiesPath))
+
+val semverMajor = properties.getProperty("semverMajor").toInt()
+val semverMinor = properties.getProperty("semverMinor").toInt()
+val semverPatch = properties.getProperty("semverPatch").toInt()
+
+val projectGroup = properties.getProperty("projectGroup") ?: ""
+val projectArtifact = properties.getProperty("projectArtifact") ?: ""
+val projectDescription = properties.getProperty("projectDescription") ?: ""
+val projectProductDimension = properties.getProperty("projectProductDimension") ?: ""
+val projectSoftwareComponent = properties.getProperty("projectSoftwareComponent") ?: ""
+
+val demo = properties.getProperty("demo") ?: ""
+val premium = properties.getProperty("premium") ?: ""
+val projectRelease = properties.getProperty("projectRelease") ?: ""
+val projectDebug = properties.getProperty("projectDebug") ?: ""
 
 plugins {
     alias(libs.plugins.android.application)
@@ -9,21 +27,27 @@ plugins {
 }
 
 android {
-    namespace = "com.idevdroidapps.project_template"
-    compileSdk = 35
+    namespace = "${projectGroup}.${projectArtifact}"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
 
     defaultConfig {
-        applicationId = "com.idevdroidapps.project_template"
-        minSdk = 28
-        targetSdk = 35
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = "$projectGroup.$projectArtifact"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = semverMajor * 1000000 +
+                semverMinor * 10000 +
+                semverPatch * 100
+        versionName = "$semverMajor.$semverMinor.$semverPatch"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
-        release {
+        getByName(projectDebug) {
+            isDefault = true
+            isMinifyEnabled = false
+        }
+        getByName(projectRelease) {
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
@@ -31,13 +55,27 @@ android {
             )
         }
     }
+
+    flavorDimensions += projectProductDimension
+    productFlavors {
+        create(premium) {
+            dimension = projectProductDimension
+        }
+        create(demo) {
+            dimension = projectProductDimension
+            isDefault = true
+        }
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
+
     buildFeatures {
         compose = true
     }
@@ -71,4 +109,10 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+fun readProperties(propertiesFile: File) = Properties().apply {
+    propertiesFile.inputStream().use { fis ->
+        load(fis)
+    }
 }
